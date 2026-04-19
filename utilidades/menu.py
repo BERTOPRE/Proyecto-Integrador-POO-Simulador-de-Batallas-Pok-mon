6a40a8b642817a11ba7ea0d41d745b3987f98d18
Alberto <mperez@gmail.com>
2026-04-19 12:27:50 -0600

    Creación de contenido de Utilidades/menu.py

--- utilidades/menu.py
+++ utilidades/menu.py
@@ -1,2 +1,7 @@
+print("==========================================")
+print("SIMULADOR DE BATALLAS POKÉMON (POO)")
+print("==========================================")
+print("1. Seleccione el modo de juego ")
+print("2. Jugador vs Jugador ")
+print("3. Jugador vs Computadora ")
 
-print("BIENVENIDO AL JUEGO DE POKÉMON")
