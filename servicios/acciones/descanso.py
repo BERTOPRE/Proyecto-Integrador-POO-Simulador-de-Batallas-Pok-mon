ed4517c962094fba2115cad4305d95e6d4f8e31b
Alberto <mperez@gmail.com>
2026-04-20 10:15:03 -0600

    Corrección de accion de descanso

--- servicios/acciones/descanso.py
+++ servicios/acciones/descanso.py
@@ -4,13 +4,17 @@ from datos.reglas_acciones import ACCIONES
 #En este servicio defino como funciona el descanso en un turno 
 class Descanso:
     def __init__(self, pokemon:Pokemon):
-        self.validacion_hp()
+        self.validacion_hp(pokemon)
         
     
-    def validacion_hp(self, pokemon:Pokemon, hp_actual):
-        if pokemon.hp_actual == 100:
-            return "No se puede aumentar la salud del pokémon, ya se encuentra con {pokemon.hp_actual}"
-        elif pokemon.hp_actual == 0:
-            pokemon.hp_actual += ACCIONES["descanso"]
-            return "HP aumentando en {hp_actual}"
+    def validacion_hp(self, pokemon:Pokemon):
+        if pokemon.hp_actual >= 100:
+            return f"No se puede aumentar la salud del pokemon, ya se encuentra con {pokemon.hp_actual}"
+
+        pokemon.hp_actual += ACCIONES["descanso"]
+
+        if pokemon.hp_actual > 100:
+            pokemon.hp_actual = 100
+
+        return f"HP aumentando a {pokemon.hp_actual}"
         
