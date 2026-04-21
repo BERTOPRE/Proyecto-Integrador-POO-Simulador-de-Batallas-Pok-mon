eab73fa468931eb70d784a5c0fbbd0703cf67757
Alberto <mperez@gmail.com>
2026-04-21 11:13:59 -0600

    Implementa ataque de PokemonAgua

--- modelos/pokemon_agua.py
+++ modelos/pokemon_agua.py
@@ -1,7 +1,17 @@
 from modelos.pokemon import Pokemon
 from datos.tipos_pokemon import TIPOS_POKEMON
 
+
 class PokemonAgua(Pokemon):
     def __init__(self, datos_pokemon):
         super().__init__(datos_pokemon["nombre"], TIPOS_POKEMON[1], datos_pokemon["hp_maximo"], datos_pokemon["energia_maxima"])
-    
+
+    def atacar(self, oponente):
+        from modelos.pokemon_fuego import PokemonFuego
+
+        multiplicador = 1
+
+        if isinstance(oponente, PokemonFuego):
+            multiplicador = 2
+
+        oponente.recibir_danio(self.calcular_danio(multiplicador))
