2aeababc4ed345f2ecd31ea4cab221b6f5d72b46
Alberto <mperez@gmail.com>
2026-04-21 11:14:20 -0600

    Implementa ataque de PokemonFuego

--- modelos/pokemon_fuego.py
+++ modelos/pokemon_fuego.py
@@ -1,6 +1,17 @@
 from modelos.pokemon import Pokemon
 from datos.tipos_pokemon import TIPOS_POKEMON
 
+
 class PokemonFuego(Pokemon):
     def __init__(self, datos_pokemon):
         super().__init__(datos_pokemon["nombre"], TIPOS_POKEMON[2], datos_pokemon["hp_maximo"], datos_pokemon["energia_maxima"])
+
+    def atacar(self, oponente):
+        from modelos.pokemon_planta import PokemonPlanta
+
+        multiplicador = 1
+
+        if isinstance(oponente, PokemonPlanta):
+            multiplicador = 2
+
+        oponente.recibir_danio(self.calcular_danio(multiplicador))
